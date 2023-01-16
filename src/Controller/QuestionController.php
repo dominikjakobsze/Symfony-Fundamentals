343d5a7717b7634a16ead026d143adcc64d34403
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-01-16 05:12:28 +0100

    Manually created annotations.yaml to use annotation routes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="app_question_homepage")
+     * @return JsonResponse
+     */
     public function homepage(): JsonResponse
     {
         return $this->json('Hello', 200);
