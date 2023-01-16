1d283c372ca891a8f4e2f10f5caf66f9c226747c
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-01-16 05:22:54 +0100

    controller Question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -16,4 +16,15 @@ class QuestionController extends AbstractController
     {
         return $this->json('Hello', 200);
     }
+
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     * @param $slug
+     * @return JsonResponse
+     */
+    public function show($slug): JsonResponse
+    {
+        $slug = trim(str_replace('-', ' ', $slug));
+        return $this->json("current url is = " . $slug, 200);
+    }
 }
