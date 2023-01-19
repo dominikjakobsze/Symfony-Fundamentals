2133b4365e7ec7637d20cb5053754f886d199efb
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-01-19 09:14:35 +0100

    added js

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comments/{id}/vote/{direction}", name="app_comment_new")
+     * @param $direction
+     * @param $id
+     * @return JsonResponse
+     */
+    public function commentVote($id, $direction): JsonResponse
+    {
+        $direction == 'up' ? $currentVoteCount = rand(7, 100) : $currentVoteCount = rand(0, 5);
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
